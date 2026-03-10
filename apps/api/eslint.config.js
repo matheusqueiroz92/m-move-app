5c122eef677cd44a2778c75bc7a13d1eff64bcd3
matheusqueiroz92 <matheus_giga@hotmail.com>
2026-03-10 00:10:51 -0300

    chore: add eslint and prettier setup

---
+++ apps/api/eslint.config.js
@@ -0,0 +1,18 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+  },
+  tseslint.configs.recommended,
+  eslintConfigPrettier,
+  simpleImportSort,
+]);
