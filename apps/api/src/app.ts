5c0bf96704b49f1d07f2a71fc300ebf61c0e4ce7
matheusqueiroz92 <matheus_giga@hotmail.com>
2026-03-11 22:58:30 -0300

    chore: add better auth setup

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,3 +1,4 @@
+import fastifyCors from "@fastify/cors";
 import fastifySwagger from "@fastify/swagger";
 import fastifySwaggerUi from "@fastify/swagger-ui";
 import fastify from "fastify";
@@ -8,6 +9,8 @@ import {
   ZodTypeProvider,
 } from "fastify-type-provider-zod";
 import z from "zod";
+
+import { auth } from "./lib/auth.js";
 
 const app = fastify({
   logger: true,
@@ -37,6 +40,13 @@ await app.register(fastifySwaggerUi, {
   routePrefix: "/docs",
 });
 
+await app.register(fastifyCors, {
+  origin: ["http://localhost:3000"],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+});
+
 app.withTypeProvider<ZodTypeProvider>().route({
   method: "GET",
   url: "/",
@@ -51,6 +61,40 @@ app.withTypeProvider<ZodTypeProvider>().route({
   },
   handler: () => {
     return { message: "Hello World" };
+  },
+});
+
+app.route({
+  method: ["GET", "POST"],
+  url: "/api/auth/*",
+  async handler(request, reply) {
+    try {
+      const url = new URL(request.url, `http:${request.headers.host}`);
+
+      const headers = new Headers();
+
+      Object.entries(request.headers).forEach(([key, value]) => {
+        if (value) headers.append(key, value.toString());
+      });
+
+      const req = new Request(url.toString(), {
+        method: request.method,
+        headers,
+        ...(request.body ? { body: JSON.stringify(request.body) } : {}),
+      });
+
+      const response = await auth.handler(req);
+
+      reply.status(response.status);
+      response.headers.forEach((value, key) => reply.header(key, value));
+      reply.send(response.body ? await response.text() : null);
+    } catch (error) {
+      app.log.error(error);
+      reply.status(500).send({
+        error: "Internal authentication error",
+        code: "AUTH FAILURE",
+      });
+    }
   },
 });
 
